c826d39abb35ac182b8e87252b31177ffeca23ef
jereto101 <jereto101@gmail.com>
2017-07-10 17:04:29 +0000

    broken destroy

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -47,15 +47,11 @@ class PortfoliosController < ApplicationController
     end
   
     def destroy
-        # Perform the lookup
-        @portfolio_item = Portfolio.find(params[:id])
-
-        # Destroy/delete the record
         @portfolio_item.destroy
-
-        # Redirect
+    
         respond_to do |format|
-            format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+            format.html { redirect_to portfolios_url, notice: 'Record was Removed.' }
+            format.json { head :no_content }
         end
     end
 
