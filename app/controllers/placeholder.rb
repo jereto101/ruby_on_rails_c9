2800a3a780c5fdfed4141575ed198d518a3470be
jereto101 <jereto101@gmail.com>
2017-07-27 17:05:57 +0000

    lots of deep stuff

--- app/controllers/placeholder.rb
+++ app/controllers/placeholder.rb
@@ -1,6 +1,2 @@
 module Placeholder
-  extend ActiveSupport::Concern
-  def self.image_generator(height:, width:)
-    "http://placehold.it/#{height}x#{width}"
-  end
 end
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,79 +1,80 @@
 class PortfoliosController < ApplicationController
-    access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
-    layout "portfolios"
-    
-    def index
-        @portfolio_items = Portfolio.by_position
-    end
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
+  layout 'portfolios'
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
   
-    def create
-        @portfolio_item = Portfolio.new(portfolio_params)
-    
-        respond_to do |format|
-            if @portfolio_item.save
-                format.html { redirect_to portfolios_path, notice: "Your portfolio item is now live." }
-            else
-                format.html { render :new }
-            end
-        end
-    end
-  
-    def show
-    end
-  
-    def sort
-        params[:order].each do |key, value|
-          Portfolio.find(value[:id]).update(position: value[:position])
-        end
-        
-        render nothing: true
-    end
-  
-    def angular
-        @angular_portfolio_items = Portfolio.angular
-    end
-  
-    def new
-        @portfolio_item = Portfolio.new
-        3.times { @portfolio_item.technologies.build }
-    end
- 
-    def edit
-        3.times { @portfolio_item.technologies.build }
+  def index
+    @portfolio_items = Portfolio.by_position
+  end
+
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
     end
 
-    def update
-        respond_to do |format|
-            if @portfolio_item.update(portfolio_params)
-                format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
-            else
-                format.html { render :edit }
-            end
-        end
-    end
-  
-    def destroy
-        @portfolio_item.destroy
-        
-        respond_to do |format|
-            format.html { redirect_to portfolios_url, notice: 'Record was Removed.' }
-            format.json { head :no_content }
-        end
-    end
+    render nothing: true
+  end
 
-    private
-    
-    def portfolio_params
-        params.require(:portfolio).permit(
-            :title,
-            :subtitle,
-            :body, 
-            technologies_attributes: [:name]
-            
-        )
-    end  
-    
-    def set_portfolio_item
-        @portfolio_item = Portfolio.find(params[:id])
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
     end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    # Destroy/delete the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      :main_image,
+                                      :thumb_image,
+                                      technologies_attributes: [:name]
+                                     )
+  end
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 end
